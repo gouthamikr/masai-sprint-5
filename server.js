4329e693c9e89939a2f19b155a57f899730095a6
kav310 <malimpatikavya@gmail.com>
2020-10-31 22:43:37 +0530

    functialities

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
+const http = require("http").createServer(app);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`The server is up and running on port ${port}`);
 });
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+//Socket
+const io = require("socket.io")(http);
+
+io.on("connection", (socket) => {
+  console.log("Connected...");
+  socket.on("message", (msg) => {
+    socket.broadcast.emit("message", msg);
+  });
 });
